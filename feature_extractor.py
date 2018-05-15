e60f5f5a10820df91f94e0988bf6bf34419e02c7
Anton Bolychev <bolychev.anton@gmail.com>
2018-05-15 09:53:22 +0300

    Added features generators tried new classification models

--- feature_extractor.py
+++ feature_extractor.py
@@ -18,7 +18,7 @@ class FeatureExtractor(object):
         # Generate mfcc means matrix MxN_MFCC
         for i, song in enumerate(self.data):
             if i % 50 == 0: print("Got mfcc for {0} songs".format(i))
-            mfcc = librosa.feature.mfcc(song, sr=sr, n_mfcc=n_mfcc)[:n_mfcc, 1:]
+            mfcc = librosa.feature.mfcc(song, sr=sr, n_mfcc=n_mfcc)
             mfcc_scaled = self.std_scaler.fit_transform(mfcc)
             mfcc_mean = mfcc_scaled.mean(axis=1)
             mfcc_means[i] = np.append(mfcc_mean, det(np.cov(mfcc_scaled, rowvar = True)))
@@ -30,12 +30,12 @@ class FeatureExtractor(object):
         return mfcc_means
 
     def generate_zero_crossing_rate(self, frame_size, hop_length):
-        zero_crossing_rates_std = np.empty((len(self.data), 1))
+        zero_crossing_rates= np.empty((len(self.data), 2))
         for i, song in enumerate(self.data):
             if i % 50 == 0: print("Got zero_cross_rate for {0} songs".format(i))
             zcr = librosa.feature.zero_crossing_rate(song)
-            zero_crossing_rates_std[i] = zcr.std()
-        return zero_crossing_rates_std
+            zero_crossing_rates[i] = np.array([zcr.mean(),zcr.std()])
+        return zero_crossing_rates
 
     def generate_centoid_meanstd(self, sr=22050):
         '''
@@ -51,15 +51,57 @@ class FeatureExtractor(object):
 
         return centroid_meanstd
     
-    def generate_rhythm(self):
-        rhythm_bpm = np.emty(len(self.data))
+    def generate_rhythm(self, sr=22050):
+        rhythm_bpm = np.empty((len(self.data), 4))
         for i, song in enumerate(self.data):
-            '''TODO'''
-            pass
-        
-            
-        
+            if i % 50 == 0: print("Got rhythm data for {0} songs".format(i))
+            onset_env = librosa.onset.onset_strength(y=song, sr=sr) 
+            tempo = librosa.beat.tempo(onset_envelope=onset_env, sr=sr)
+            dtempo = librosa.beat.tempo(onset_envelope=onset_env, sr=sr, aggregate=None)
+            rhythm_bpm[i, :] = np.array([tempo, dtempo.mean(), dtempo.std(), len(np.unique(dtempo))]) 
+        return rhythm_bpm
+    
+    def generate_rolloff(self, sr=22050):
+        '''
+            Returns ndarray with 2 columns: 
+                First column:  mean
+                Second column: std
+        '''
+        rolloff_meanstd = np.empty((len(self.data), 2))
+        for i, song in enumerate(self.data):
+            if i % 50 == 0: print("Got rolloff data for {0} songs".format(i))
+            rolloff = librosa.feature.spectral_rolloff(y=song, sr=sr, roll_percent=0.85)[0]
+            rolloff_meanstd[i, :] = np.array([rolloff.mean(), rolloff.std()]) 
 
+        return rolloff_meanstd
+
+    def generate_flux(self, sr=22050):
+        '''
+            Returns ndarray with 2 columns: 
+                First column:  mean
+                Second column: std
+        '''
+        flux_meanstd = np.empty((len(self.data), 2))
+        for i, song in enumerate(self.data):
+            if i % 50 == 0: print("Got flux data for {0} songs".format(i))
+            rmse = librosa.feature.rmse(y=song)
+            max_rmse = max(rmse)
+            rmse = np.where(rmse == 0, 1, rmse)
+            D = librosa.stft(y=song) / rmse
+            flux = np.sqrt((np.abs((D[:, 1:] - D[:, :-1]))**2).sum(axis=0))
+            flux_meanstd[i, :] = np.array([flux.mean(), flux.std()])
+
+        return flux_meanstd
+
+    def generate_energy(self, sr=22050):
+        low_energy = np.empty(len(self.data))
+        for i, song in enumerate(self.data):
+            if i % 50 == 0: print("Got rmse data for {0} songs".format(i))
+            rmse = librosa.feature.rmse(y=song)
+            mean_rmse = rmse.mean()
+            low_energy[i] = np.sum(rmse <= mean_rmse) / len(rmse)
+        
+        return low_energy.reshape(-1,1)
 
 if __name__ == '__main__':
     db = PostgresqlWrapper()
